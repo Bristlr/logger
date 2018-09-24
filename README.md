8a1fda21b5149b7fd16c3bb7f3945af7e63236a6
John <john@m14.industries>
2018-09-24 08:41:57 +0100

    Log for DataDog to read custom metrics from CloudWatch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # logger
 Custom logging to handle measurements and counts.
+
+If configured, send custom event to CloudWatch
--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 module.exports = function(injectedConsoleLog) {
 
-	var consoleLog = injectedConsoleLog || console.log;
+	const consoleLog = injectedConsoleLog || console.log;
 
 	function count(property, value) {
 		if (!value) value = 1;
 		consoleLog("count#" + property + "=" + value);
+		cloudWatchLog("count", property, value);
 	}
 
 	function measure(property, value) {
 		consoleLog("measure#" + property + "=" + value);
+		cloudWatchLog("measure", property, value);
 	}
 
 	function time(property, value) {
 		consoleLog("measure#" + property + "=" + value + "ms");
+		cloudWatchLog("measure", property, value + "ms");
+	}
+
+	function cloudWatchLog(type, property, value) {
+		const timestamp = new Date().getTime();
+		console.log(`MONITORING|${timestamp}|${value}|${type}|${property}|#tag1:lambda`);
 	}
 
 	return {
