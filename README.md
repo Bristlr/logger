8a41f655c515eebde31d8816d25609377bf14b32
John <john@m14.industries>
2019-06-13 12:40:55 +0100

    Only use desired monitoring log output, and support tags

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # logger
+
 Custom logging to handle measurements and counts.
 
 If configured, send custom event to CloudWatch
+
+## Usage Examples
+
+```js
+
+const l = new require("bristlr-logger")();
+
+l.count("accountCreated");
+
+l.count("accountCreated", 2);
+
+l.count("accountCreated", 2, "tag");
+
+l.count("accountCreated", 2, ["multiple","tags"]);
+
+l.measure("someStat", 100);
+
+l.measure("someStat", 100, ["multiple","tags"]);
+
+l.time("someStat", 100);
+
+```
--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-module.exports = function(injectedConsoleLog) {
+module.exports = function(injectedConsoleLog, injectedTimestamp) {
 
 	const consoleLog = injectedConsoleLog || console.log;
 
-	function count(property, value) {
-		if (!value) value = 1;
-		consoleLog("count#" + property + "=" + value);
-		cloudWatchLog("count", property, value);
+	function count(property, value, tags) {
+		if (!value) {
+			value = 1;
+		}
+		cloudWatchLog("count", property, value, tags);
 	}
 
-	function measure(property, value) {
-		consoleLog("measure#" + property + "=" + value);
-		cloudWatchLog("measure", property, value);
+	function measure(property, value, tags) {
+		cloudWatchLog("measure", property, value, tags);
 	}
 
-	function time(property, value) {
-		consoleLog("measure#" + property + "=" + value + "ms");
-		cloudWatchLog("measure", property, value + "ms");
+	function time(property, value, tags) {
+		cloudWatchLog("measure", property, value + "ms", tags);
 	}
 
-	function cloudWatchLog(type, property, value) {
-		const timestamp = Math.round(new Date().getTime()/1000);
-		console.log(`MONITORING|${timestamp}|${value}|${type}|${property}|#tag1:lambda`);
+	function cloudWatchLog(type, property, value, tags) {
+
+		const timestamp = injectedTimestamp || Math.round(new Date().getTime() / 1000);
+		const logTags = getLogTags(tags);
+
+		consoleLog(`MONITORING|${timestamp}|${value}|${type}|${property}|${logTags}`);
+	}
+
+	function getLogTags(tags) {
+
+		const logTags = [];
+
+		if (tags) {
+			if (typeof tags == "string") {
+				logTags.push("tags:" + tags);
+			}
+			if (Array.isArray(tags)) {
+				tags.forEach(function(tag) {
+					logTags.push("tags:" + tag);
+				});
+			}
+		}
+
+		if (process.env.AWS_LAMBDA_FUNCTION_NAME) {
+			logTags.push("tags:lambda");
+			logTags.push("tags:lambdaFunction." + process.env.AWS_LAMBDA_FUNCTION_NAME);
+		}
+
+		if (logTags.length == 0) {
+			return "";
+		}
+
+		return logTags.join(",");
 	}
 
 	return {
-		count: count,
-		measure: measure,
-		time: time
+		count,
+		measure,
+		time
 	};
 
 };
